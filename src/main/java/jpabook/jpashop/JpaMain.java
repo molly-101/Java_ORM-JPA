5d06a5e45f98392c28e8eac9afe496b6f5f6b37b
wotj7687 <wotj7687@icloud.com>
2021-05-19 00:28:58 +0900

    Setting Object to Relational DB

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,25 +1,18 @@
-package hellojpa;
+package jpabook.jpashop;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("C");
-
-            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
@@ -27,7 +20,7 @@ public class JpaMain {
         } finally {
             em.close();
         }
-        emf.close();
 
+        emf.close();
     }
 }
